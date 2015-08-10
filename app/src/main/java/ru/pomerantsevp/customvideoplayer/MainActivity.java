365ed8054bf2a1cb62ef2af4742587955e45eb5d
Pavel Pomerantsev <pomerantsevp@gmail.com>
2015-08-10 13:47:40 +0300

    Successfully copy code for playing video

--- app/src/main/java/ru/pomerantsevp/customvideoplayer/MainActivity.java
+++ app/src/main/java/ru/pomerantsevp/customvideoplayer/MainActivity.java
@@ -1,17 +1,55 @@
 package ru.pomerantsevp.customvideoplayer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Matrix;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements TextureView.SurfaceTextureListener,
+        MoviePlayer.PlayerFeedback {
+
+    public static final String TAG = "Custom Video Player";
+
+    private TextureView mTextureView;
+    private boolean mShowStopLabel;
+    private MoviePlayer.PlayTask mPlayTask;
+    private boolean mSurfaceTextureReady = false;
+
+    private final Object mStopper = new Object();   // used to signal stop
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Log.v(TAG, getExternalMediaDirs(Environment.DIRECTORY_MOVIES).toString());
+//        for (File file : getExternalFilesDir(Environment.DIRECTORY_MOVIES).listFiles()) {
+//            Log.v(TAG, file.getName());
+//        }
+//        File videoFile = new File(Uri.parse("content://com.google.android.apps.photos.contentprovider/0/2/content%3A%2F%2Fmedia%2Fexternal%2Fvideo%2Fmedia%2F6172/ACTUAL/2008344195"));
+//        File videoDirectory = new File(MediaStore.Video.Media.EXTERNAL_CONTENT_URI.getPath());
+//        for (File file : videoDirectory.listFiles()) {
+//            Log.v(TAG, file.getName());
+//        }
+//        Log.v(TAG, MediaStore.Video.Media.EXTERNAL_CONTENT_URI.getPath());
+        Log.v(TAG, new File(Environment.getExternalStorageDirectory(), "long-video.mp4").getName());
+
+        mTextureView = (TextureView) findViewById(R.id.movie_texture_view);
+        mTextureView.setSurfaceTextureListener(this);
+
+        updateControls();
     }
 
     @Override
@@ -34,5 +72,158 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "PlayMovieActivity onPause");
+        super.onPause();
+        // We're not keeping track of the state in static fields, so we need to shut the
+        // playback down.  Ideally we'd preserve the state so that the player would continue
+        // after a device rotation.
+        //
+        // We want to be sure that the player won't continue to send frames after we pause,
+        // because we're tearing the view down.  So we wait for it to stop here.
+        if (mPlayTask != null) {
+            stopPlayback();
+            mPlayTask.waitForStop();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture st, int width, int height) {
+        // There's a short delay between the start of the activity and the initialization
+        // of the SurfaceTexture that backs the TextureView.  We don't want to try to
+        // send a video stream to the TextureView before it has initialized, so we disable
+        // the "play" button until this callback fires.
+        Log.d(TAG, "SurfaceTexture ready (" + width + "x" + height + ")");
+        mSurfaceTextureReady = true;
+        updateControls();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture st, int width, int height) {
+        // ignore
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture st) {
+        mSurfaceTextureReady = false;
+        // assume activity is pausing, so don't need to update controls
+        return true;    // caller should release ST
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // ignore
+    }
+
+    /**
+     * onClick handler for "play"/"stop" button.
+     */
+    public void clickPlayStop(@SuppressWarnings("unused") View unused) {
+        if (mShowStopLabel) {
+            Log.d(TAG, "stopping movie");
+            stopPlayback();
+            // Don't update the controls here -- let the task thread do it after the movie has
+            // actually stopped.
+            //mShowStopLabel = false;
+            //updateControls();
+        } else {
+            if (mPlayTask != null) {
+                Log.w(TAG, "movie already playing");
+                return;
+            }
+            Log.d(TAG, "starting movie");
+            SpeedControlCallback callback = new SpeedControlCallback();
+//            if (((CheckBox) findViewById(R.id.locked60fps_checkbox)).isChecked()) {
+//                // TODO: consider changing this to be "free running" mode
+//                callback.setFixedPlaybackRate(60);
+//            }
+            SurfaceTexture st = mTextureView.getSurfaceTexture();
+            Surface surface = new Surface(st);
+            MoviePlayer player = null;
+            try {
+                player = new MoviePlayer(
+                        new File(Environment.getExternalStorageDirectory(), "long-video.mp4"), surface, callback);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Unable to play movie", ioe);
+                surface.release();
+                return;
+            }
+            adjustAspectRatio(player.getVideoWidth(), player.getVideoHeight());
+
+            mPlayTask = new MoviePlayer.PlayTask(player, this);
+//            if (((CheckBox) findViewById(R.id.loopPlayback_checkbox)).isChecked()) {
+            mPlayTask.setLoopMode(true);
+//            }
+
+            mShowStopLabel = true;
+            updateControls();
+            mPlayTask.execute();
+        }
+    }
+
+    /**
+     * Requests stoppage if a movie is currently playing.  Does not wait for it to stop.
+     */
+    private void stopPlayback() {
+        if (mPlayTask != null) {
+            mPlayTask.requestStop();
+        }
+    }
+
+    @Override   // MoviePlayer.PlayerFeedback
+    public void playbackStopped() {
+        Log.d(TAG, "playback stopped");
+        mShowStopLabel = false;
+        mPlayTask = null;
+        updateControls();
+    }
+
+    /**
+     * Sets the TextureView transform to preserve the aspect ratio of the video.
+     */
+    private void adjustAspectRatio(int videoWidth, int videoHeight) {
+        int viewWidth = mTextureView.getWidth();
+        int viewHeight = mTextureView.getHeight();
+        double aspectRatio = (double) videoHeight / videoWidth;
+
+        int newWidth, newHeight;
+        if (viewHeight > (int) (viewWidth * aspectRatio)) {
+            // limited by narrow width; restrict height
+            newWidth = viewWidth;
+            newHeight = (int) (viewWidth * aspectRatio);
+        } else {
+            // limited by short height; restrict width
+            newWidth = (int) (viewHeight / aspectRatio);
+            newHeight = viewHeight;
+        }
+        int xoff = (viewWidth - newWidth) / 2;
+        int yoff = (viewHeight - newHeight) / 2;
+        Log.v(TAG, "video=" + videoWidth + "x" + videoHeight +
+                " view=" + viewWidth + "x" + viewHeight +
+                " newView=" + newWidth + "x" + newHeight +
+                " off=" + xoff + "," + yoff);
+
+        Matrix txform = new Matrix();
+        mTextureView.getTransform(txform);
+        txform.setScale((float) newWidth / viewWidth, (float) newHeight / viewHeight);
+        //txform.postRotate(10);          // just for fun
+        txform.postTranslate(xoff, yoff);
+        mTextureView.setTransform(txform);
+    }
+
+    /**
+     * Updates the on-screen controls to reflect the current state of the app.
+     */
+    private void updateControls() {
+        Button play = (Button) findViewById(R.id.play_stop_button);
+        if (mShowStopLabel) {
+            play.setText(R.string.stop_button_text);
+        } else {
+            play.setText(R.string.play_button_text);
+        }
+        play.setEnabled(mSurfaceTextureReady);
     }
 }
